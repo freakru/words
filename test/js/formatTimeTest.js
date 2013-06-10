92312fe11e81a2d4b0ea7b42fe9b6ddc734dc734
ogiese <o.giese@gmail.com>
2013-06-10 22:11:02 +0200

    qunit tests updated

---
+++ test/js/formatTimeTest.js
@@ -0,0 +1,33 @@
+test('Timer', function() {
+
+  function time(val, expected, message) {
+    equal(val.formatTime(), expected, message);
+  }
+  
+  var val = 0;
+  time(val, '00:00:00', 'Null time');
+  
+  val = 59;
+  time(val, '00:00:59', '59 seconds');
+  
+  val = 60;
+  time(val, '00:01:00', '1 minute');
+  
+  val = 30 * 60;
+  time(val, '00:30:00', '30 minutes');
+  
+  val = 60 * 60;
+  time(val, '01:00:00', '1 hour');
+
+  val = 60 * 60 + (4 *60) + 44;
+  time(val, '01:04:44', '1 hour 4 min 44 sek');
+  
+  val = 60 * 60 + 48 * 60 + 25;
+  time(val, '01:48:25', '1 hour 48 min 25 sec');
+  
+  val = 99;
+  time(val, '00:01:39', '1 min 39 sec');
+  
+  val = -1;
+  time(val, '00:00:00', 'ignore negative');
+});
---
+++ test/js/updateLevelBarTest.js
@@ -0,0 +1,58 @@
+module('updateLevelBar', {
+  setup: function() {
+    var $fixture = $('#qunit-fixture');
+    var $levelBar = $fixture.append('<div id="level-bar"><div class="level-chunk"></div></div>');
+  },
+
+  teardown: function() {
+    
+  }
+});
+
+
+test( "simple", function() {
+  
+  var $fixture = $('#qunit-fixture');
+  equal( $( "#level-bar", $fixture ).width(), 480, "level-bar added successfully!" );
+});
+ 
+test( "level 1", function() {
+  var $fixture = $('#qunit-fixture');
+  var $bar = $('#level-bar .level-chunk');
+  var graphic = new Graphic();
+
+  graphic.updateLevelBar(1, 0, 50);
+
+  equal( $bar.width(), 0, "right width is 0" );
+});
+
+test( "level 1 score 100", function() {
+  var $fixture = $('#qunit-fixture');
+  var $bar = $('#level-bar .level-chunk');
+  var graphic = new Graphic();
+
+  graphic.updateLevelBar(1, 100, 50);
+
+  equal( $bar.width(), 100, "right width is 100" );
+});
+
+
+test( "level 1 score 490", function() {
+  var $fixture = $('#qunit-fixture');
+  var $bar = $('#level-bar .level-chunk');
+  var graphic = new Graphic();
+
+  graphic.updateLevelBar(1, 490, 50);
+
+  equal( $bar.width(), 10, "right width is 10" );
+});
+
+test( "level 2 score 680", function() {
+  var $fixture = $('#qunit-fixture');
+  var $bar = $('#level-bar .level-chunk');
+  var graphic = new Graphic();
+
+  graphic.updateLevelBar(2, 680, 50);
+
+  equal( $bar.width(), 340, "right width is 340" );
+});
